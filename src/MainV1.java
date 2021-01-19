215a63bc8bf71b501edcbff142f4aac679e509aa
Olga Tarasova <bos.olga18@gmail.com>
2021-01-19 15:21:14 +0300

    Added files

---
+++ src/MainV1.java
@@ -0,0 +1,11 @@
+public class MainV1 {
+    public static void main(String[] args){
+        double ticket = 2150.70;
+        int mile = 20_00;
+
+        double amountmile = ticket / mile;
+        System.out.println(amountmile);
+
+    }
+
+}
---
+++ src/MainV2.java
@@ -0,0 +1,10 @@
+public class MainV2 {
+    public static void main(String[] args){
+        long ticket = 2150_00;
+        int mile = 20_00;
+
+        int amountmile = (int) (ticket / mile);
+        System.out.println(amountmile);
+
+    }
+}
